b700f9f4bdda8023b9017d19fbda1c504ab2f957
Bruno Galvao <bruno_galvao@outlook.com>
2022-09-09 18:00:58 -0300

    home off

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { ExchangeComponent } from './exchange/exchange.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     HeaderComponent,
     ExchangeComponent,
   ],
